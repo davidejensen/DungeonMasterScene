242e2663d0165cd1cc5ea58b03798c65b9b579f1
Alvaro Luque <olavra@live.com>
2023-05-18 19:26:38 +0200

    Create character prompt

--- src/prompts/create_character.ts
+++ src/prompts/create_character.ts
@@ -1,48 +1,19 @@
-export const createCharacterPrompt = `
-I want you to be a dungeon master for my role game. The game is setup in fantasy world similar to Dungeons and Dragons or Lord of the Rings.
+export const createCharacterPrompt = 
+`
+I want you to be a dungeon master for my role game. The game is setup in fantasy world similar to Dungeons and Dragons.
+You will use JSON as format for your output. 
 
-You will use JSON as format for your output. Your output will be formatted in the following format
-{"action": "ACTION", message:"MESSAGE"}
-I will send you messages in the following format:
-{"action": "ACTION", message:"MESSAGE"}
-
-CHARACTER CREATION
-- To create a character I will send you an action "choose_character" selecting from "WARRIOR", "MAGE" or "ROGUE" in the value parameter
-- You will them create a character with the following structure
+These are the rules to create a character for our players. 
+Characters have the following structure
 -- name
 -- bio: You will create a short description of the characters biography
--- life
+-- life: always 100
 -- class
--- ability: You will describe the name and the effect of the special ability of the character.
--- strength
--- intelligence
--- dexterity
--- charisma
+-- str: for WARRIOR (70-80), for MAGE(20-30)
+-- int: for WARRIOR (20-30), for MAGE(70-80)
 
-- You will randomize the stats of attack and defense based on the selected character class:
-- WARRIOR
--- life: 100
--- strength: 70-80
--- intelligence: 10-20
--- dexterity: 40-50
--- charisma: 20-40
-- MAGE
--- life: 70
--- life: 100
--- strength: 20-30
--- intelligence: 70-90
--- dexterity: 30-60
--- charisma: 40-90
-- ROGUE
--- life: 70
--- life: 100
--- strength: 40-60
--- intelligence: 40-60
--- dexterity: 70-90
--- charisma: 50-70
+You will return the character requested in the following JSON data structure:
+{"action":"character", "character":{CHARACTER_DATA}}
 
-This data structure will be returned in the "message" parameter of your output with the action "character_created".
-You will keep the last character you created as the "Player character".
-
-If you understood this rules, reply with the action: ready and message: yes
+I want you to create a WARRIOR character
 `
