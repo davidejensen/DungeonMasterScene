36b19d6498116eca5dffe033f8ccdc4086d16137
davidejensen <davidejensen@live.it>
2023-05-18 12:08:27 +0200

    Added test prompt

--- src/ui.tsx
+++ src/ui.tsx
@@ -15,8 +15,8 @@ let currentTextString: string;
 const uiComponent = () => (
   <UiEntity
     uiTransform={{
-      width: 400,
-      height: 230,
+      width: 700,
+      height: 100,
       //  { top: 16, right: 0, bottom: 8 left: 270 },
       margin: '16px 0 8px 270px',
       // { top: 4, bottom: 4, left: 4, right: 4 },
@@ -35,37 +35,24 @@ const uiComponent = () => (
       uiBackground={{ color: Color4.fromHexString("#70ac76ff") }}
     >
       <Input
-        onChange={(e) =>{ currentTextString = "you wrote: " + e }}
-        fontSize={35}
+        onChange={(e) =>{ currentTextString = e }}
+        fontSize={12}
         placeholder={"type something"}
         placeholderColor={Color4.Gray()}
       />
-      <UiEntity
-        uiTransform={{
-          width: '100%',
-          height: 50,
-          margin: '8px 0'
-        }}
-        uiBackground={{
-          textureMode: 'center',
-          texture: {
-            src: 'images/scene-thumbnail.png',
-          },
-        }}
-        uiText={{ value: 'DungeonMaster', fontSize: 18 }}
-      />
-      <Label
-      onMouseDown={()=>{generateNewRoom(Math.random() < 0.5, Math.random() < 0.5, Math.random() < 0.5, Math.random() < 0.5)}}
-        value={`Player: ${getPlayerPosition()}`}
-        fontSize={18}
-        uiTransform={{ width: '100%', height: 30 } }
+      <Button
+        onMouseDown={()=>{sendPrompt()}}
+        uiTransform={{ width: 120, height: 40, margin: 8 }}
+        value='Send'
+        variant='primary'
+        fontSize={14}
       />
       <Button
         onMouseDown={()=>{generateNewRoom(Math.random() < 0.5, Math.random() < 0.5, Math.random() < 0.5, Math.random() < 0.5)}}
-        uiTransform={{ width: 120, height: 40, margin: 8 }}
+        uiTransform={{ width: 80, height: 20, margin: 4 }}
         value='New room'
         variant='primary'
-        fontSize={14}
+        fontSize={8}
       />
      </UiEntity>
   </UiEntity>
@@ -77,6 +64,11 @@ function getPlayerPosition() {
   if (!playerPosition) return ' no data yet'
   const { x, y, z } = playerPosition.position
   return `{X: ${x.toFixed(2)}, Y: ${y.toFixed(2)}, z: ${z.toFixed(2)} }`
+}
+
+function sendPrompt()
+{
+  console.log(currentTextString)
 }
 
 async function testRequest(){
